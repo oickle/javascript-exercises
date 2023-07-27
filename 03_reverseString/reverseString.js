9f0ee5691cac8f388196753c20d0cf4565573d46
oickle <oycell@aol.com>
2023-07-27 19:02:04 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    // My original solution
+    /*
+    let reversedInput = "";
+    let inputArray = input.split("");
+    for (let i = 0; i < inputArray.length; i++) {
+        reversedInput = inputArray[i] + reversedInput;
+    }
 
+    return reversedInput;
+    */
+
+    // The better solution
+    return input.split("").reverse().join("")
 };
+
+reverseString("dog");
 
 // Do not edit below this line
 module.exports = reverseString;
