17c15eb06fb937edc74e33dcf6ae259baa17684f
oickle <oycell@aol.com>
2023-07-27 18:50:23 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+    let output = "";
 
+    if (count < 0) return "ERROR";
+    
+    for (i = 0; i < count; i++) {
+        output += word;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
