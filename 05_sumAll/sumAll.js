fe76019d25110a1b3a94f08cf04c4f55f22cde6b
oickle <oycell@aol.com>
2023-07-27 20:33:10 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Prevents sum from being competely negative by preventing any negatives as parameters
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") return "ERROR";
 
+    // Sets start and end based on which value is lesser or greater
+    let start = num1 < num2 ? num1 : num2;
+    let end = num1 > num2 ? num1 : num2;
+
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
