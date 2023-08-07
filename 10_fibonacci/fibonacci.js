38fdb0aa331f2db86c410545dd50d271ccd0f0d3
oickle <oycell@aol.com>
2023-08-07 14:37:44 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if(+index < 0) return "OOPS";
+    
+    let a = 0;
+    let b = 1;
+    let series = [a, b]; // sets first two values
 
+    for(let i = 1; i < +index; i++) {
+        let sum = a + b;
+        series.push(sum);
+        
+        // moves everything to the right one
+        a = b;
+        b = sum;
+    }
+    
+    // returns last value in array
+    return series.pop();
 };
 
 // Do not edit below this line
