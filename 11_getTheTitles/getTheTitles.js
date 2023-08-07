200b388c56cbabd4c99911c66a62684149e79cd8
oickle <oycell@aol.com>
2023-08-07 16:18:41 -0400

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
 
+const getTheTitles = function(array) {
+    // Due to it be an arrow function, automatically returns what is 
+    // Map runs through every item in array and creates a new array with desires values (book.title in this case)
+    const titles = array.map((book) => book.title);
+    return titles; // could be one line but I didn't want it to be :)
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
