0ec8730f65b027657b03da77a95110ed663ecee7
oickle <oycell@aol.com>
2023-08-07 13:39:58 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  // Doesn't need a default value
+  return numbers.reduce((a, b) => {return a * b});
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+
+  let final = 1;
+	for(let i = 1; i <= a; i++) {
+    final *= i;
+  }
+  return final;
 };
 
 // Do not edit below this line
