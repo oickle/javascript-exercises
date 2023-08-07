345580b975f478448afb1229c69dd7807a509367
oickle <oycell@aol.com>
2023-08-07 14:09:16 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const clean = sentence
+    .toLowerCase()
+    .replaceAll(/[\.,?!]/g, "") // replaces punctuation
+    .replaceAll(' ', ''); // replaces spaces
 
+    let reversed = clean
+    .split('') // turns into array
+    .reverse() // flips array
+    .join(''); // turns back into string
+
+    return clean === reversed;
 };
 
 // Do not edit below this line
